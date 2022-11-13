14466995789c8c37873f1b0222f069ca569e0904
veras92 <110496079+veras92@users.noreply.github.com>
2022-11-13 21:32:20 +0000

    Deploying to gh-pages from @ veras92/i-scream@439e15086909ca9323ec997282575e5f89dc94b4 🚀

---
+++ index.07c899fd.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-about-scream-modal-open]"),closeModalBtn:document.querySelector("[data-about-scream-modal-close]"),modal:document.querySelector("[data-about-scream-modal]")};function o(){document.body.classList.toggle("modal-open"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.07c899fd.js.map
